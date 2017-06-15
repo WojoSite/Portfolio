c81fbe1b087559088e7eae271ab5a02deef3ed95
Wojo <matt.wojtkun@gmail.com>
2017-06-14 22:54:00 -0400

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+$(document).ready(function($) {
+  console.log("jquery ready");
 
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    $('.container').css('height', windowHeight);
+  };
+  setHeight();
+  $(window).resize(function() {
+    setHeight();
+  });
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 300) {
+      $('#header').fadeIn();
+    } else {
+      $('#header').fadeOut();
+    }
+  });
+
+});
