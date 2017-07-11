066d189fbb923f1f91cde5c539b23bc67fab46a7
Wojo <matt.wojtkun@gmail.com>
2017-07-10 20:28:22 -0400

    added styles

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function($) {
   console.log("jquery ready");
-
   function setHeight() {
     windowHeight = $(window).innerHeight();
     $('.container').css('height', windowHeight);
@@ -9,16 +8,12 @@ $(document).ready(function($) {
   $(window).resize(function() {
     setHeight();
   });
-
   $(document).scroll(function() {
     var y = $(this).scrollTop();
     if (y > 175) {
-      $('#header').fadeIn();
       $('#splash-item-container').fadeOut();
     } else {
-      $('#header').fadeOut();
       $('#splash-item-container').fadeIn();
     }
   });
-
 });
