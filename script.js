cc9bf2b41a559b353c0f2820b09c780807f447b7
Wojo <matt.wojtkun@gmail.com>
2017-08-27 19:49:34 -0400

    refactored ScrollMagic script

--- script.js
+++ script.js
@@ -11,67 +11,13 @@ $(document).ready(function($) {
   // ScrollMagic controller init
   var controller = new ScrollMagic.Controller();
 
-  // Tweens
-  var studyStacksTween = new TweenMax.to('#study-stacks-font', 1, {
-    opacity: 1
+  // loop through .display-text items and add scene
+  $('.display-text').each(function(){
+    var myScene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.8
+    })
+    .setClassToggle(this, "fade-in")
+    .addTo(controller);
   });
-  var wheelOfLuckTween = new TweenMax.to('#wheel-of-luck-font', 1, {
-    opacity: 1
-  });
-  var weatherAppTween = new TweenMax.to('#weather-app-font', 1, {
-    opacity: 1
-  });
-  var aboutTween = new TweenMax.to('.about-text', 1, {
-    opacity: 1
-  });
-  var contactTween = new TweenMax.to('#contact-text', 1, {
-    opacity: 1
-  });
-
-  // Scenes
-  var studyStacksScene = new ScrollMagic.Scene({
-    triggerElement: "#study-stacks-font",
-    triggerHook: 0.8
-  })
-  .setTween(studyStacksTween)
-  .addIndicators({
-      name: "Study Stacks"
-  })
-  .addTo(controller);
-  var wheelOfLuckScene = new ScrollMagic.Scene({
-    triggerElement: "#wheel-of-luck-font",
-    triggerHook: 0.8
-  })
-  .setTween(wheelOfLuckTween)
-  // .addIndicators({
-  //   name: "Wheel of Luck"
-  // })
-  .addTo(controller);
-  var weatherAppScene = new ScrollMagic.Scene({
-    triggerElement: "#weather-app-font",
-    triggerHook: 0.8
-  })
-  .setTween(weatherAppTween)
-  // .addIndicators({
-  //   name: "Weather App"
-  // })
-  .addTo(controller);
-  var contactScene = new ScrollMagic.Scene({
-    triggerElement: "#contact-text",
-    triggerHook: 0.8
-  })
-  .setTween(contactTween)
-  // .addIndicators({
-  //     name: "Contact"
-  // })
-  .addTo(controller);
-  var aboutScene = new ScrollMagic.Scene({
-    triggerElement: ".about-text",
-    triggerHook: 0.8
-  })
-  .setTween(aboutTween)
-  // .addIndicators({
-  //     name: "About"
-  // })
-  .addTo(controller);
 });
